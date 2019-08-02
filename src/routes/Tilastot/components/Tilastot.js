caed3c1b487d24d88516484be47efcd9ea373c0e
Sami Heikkinen <sami.heikkinen@valamis.com>
2019-08-02 10:44:38 +0300

    Tilastojen linkit päivitetty ja lisätty otsikko

--- src/routes/Tilastot/components/Tilastot.js
+++ src/routes/Tilastot/components/Tilastot.js
@@ -17,17 +17,6 @@ const Title = styled.h1`
   }
 `
 
-const Description = styled.p`
-  font-size: 18px;
-  max-width: 500px;
-  margin: 50px 0 0 20px;
-  position: relative;
-  
-  @media ${MEDIA_QUERIES.MOBILE} {
-    margin: 15px;
-  }
-`
-
 const Linkki = styled.div`
     margin-top:15px;  
 `
@@ -37,14 +26,16 @@ class Tilastot extends Component {
     return (
       <ContentContainer>
         <Helmet height="50px">
-           <Title>Oiva | Tilastot</Title>
+          <Title>Oiva | Tilastot</Title>
         </Helmet>
-          <p>
-            Linkkejä tilastodataan (beta-versio):
-          </p>
-          <Linkki><a href="https://app.powerbi.com/view?r=eyJrIjoiN2M1YWZiYzUtOTEyYS00NTY2LTgzNDctNGZjOTVmOWQ4ZTlkIiwidCI6IjkxMDczODlkLTQ0YjgtNDcxNi05ZGEyLWM0ZTNhY2YwMzBkYiIsImMiOjh9" target="_blank">Ammatillisen koulutuksen järjestämisluvat</a></Linkki>
-          <Linkki><a href="https://app.powerbi.com/view?r=eyJrIjoiNjQ4NGRmNWEtYmRmMC00YWNkLWI4NGEtNDg2NTBmMWFmYTM0IiwidCI6IjkxMDczODlkLTQ0YjgtNDcxNi05ZGEyLWM0ZTNhY2YwMzBkYiIsImMiOjh9" target="_blank">Väestötiedot</a></Linkki>
-
+        <h1>Tilastot</h1>
+        <p>
+          Linkkejä tilastodataan (beta-versio):
+        </p>
+        <Linkki><a href="https://app.powerbi.com/view?r=eyJrIjoiN2M1YWZiYzUtOTEyYS00NTY2LTgzNDctNGZjOTVmOWQ4ZTlkIiwidCI6IjkxMDczODlkLTQ0YjgtNDcxNi05ZGEyLWM0ZTNhY2YwMzBkYiIsImMiOjh9" target="_blank" rel="noopener noreferrer">Ammatillisen koulutuksen järjestämisluvat</a></Linkki>
+        <Linkki><a href="https://app.powerbi.com/view?r=eyJrIjoiNWNmNTU0MzgtOTljYS00ZjNlLTljOGQtMWQ0YWZjMGU2MDliIiwidCI6IjkxMDczODlkLTQ0YjgtNDcxNi05ZGEyLWM0ZTNhY2YwMzBkYiIsImMiOjh9" target="_blank" rel="noopener noreferrer">Väestöennuste</a></Linkki>
+        <Linkki><a href="https://app.powerbi.com/view?r=eyJrIjoiN2Q2M2EwMjctMDU5Mi00YjFiLWE5MjItN2ExMDE1YjFlYTQyIiwidCI6IjkxMDczODlkLTQ0YjgtNDcxNi05ZGEyLWM0ZTNhY2YwMzBkYiIsImMiOjh9" target="_blank" rel="noopener noreferrer">Väestö äidinkielen mukaan</a></Linkki>
+        <Linkki><a href="https://app.powerbi.com/view?r=eyJrIjoiMDI5MjMzZTAtZjVkYy00NTZkLTk1NDUtZDAxMDFkNjUwODJlIiwidCI6IjkxMDczODlkLTQ0YjgtNDcxNi05ZGEyLWM0ZTNhY2YwMzBkYiIsImMiOjh9" target="_blank" rel="noopener noreferrer">Koulutus ja pääasiallinen toiminta</a></Linkki>
       </ContentContainer>
     )
   }
