b158df44174d9fbbb99ee9803e7f7421d8f37f28
Sami Heikkinen <sami.heikkinen@valamis.com>
2020-03-30 11:47:28 +0300

    {/* <Palaute><a href=https://link.webropolsurveys.com/S/F156FEC089139BF4 target=_blank>Palautelomake</a></Palaute> */}

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import { Helmet } from 'react-helmet'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
 
-import Hero from './Hero'
-import LinkBox from './LinkBox'
-import InfoBox from './InfoBox'
-import { BackgroundImage, MEDIA_QUERIES } from 'modules/styles'
-import { ContentContainer } from '../../../modules/elements'
+import Hero from "./Hero";
+import LinkBox from "./LinkBox";
+import InfoBox from "./InfoBox";
+import { BackgroundImage, MEDIA_QUERIES } from "modules/styles";
+import { ContentContainer } from "../../../modules/elements";
 
 const Title = styled.h2`
   color: #555;
   margin: 60px 0 0 20px;
   position: relative;
   top: 20px;
-  
+
   @media ${MEDIA_QUERIES.MOBILE} {
     margin: 15px;
     top: 0;
   }
-`
+`;
 
 const Palaute = styled.div`
-    margin-top:25px;  
-`
-
+  margin-top: 25px;
+`;
 
 class Home extends Component {
   render() {
@@ -37,13 +36,17 @@ class Home extends Component {
         </Hero>
         <InfoBox maxWidth="600px">
           <p>
-            Palvelusta löytyvät kaikki ajantasaiset ammatillisen koulutuksen järjestämisluvat. Palvelu tulee sisältämään hakemusten laatimisen ja päätöksenteon lisäksi väestö-, koulutus- ja lupatietoja ja erilaisia tilastoja. Myöhemmin Oiva laajennetaan koskemaan myös yleissivistävän koulutuksen järjestämisluvat ja ylläpitämisluvat ja lupien hakupalvelut.
+            Palvelusta löytyvät tällä hetkellä kaikki ajantasaiset ammatillisen
+            koulutuksen järjestämisluvat sekä vapaan sivistystyön oppilaitosten
+            ylläpitämisluvat. Myöhemmin Oiva laajennetaan koskemaan myös esi- ja
+            perusopetuksen sekä lukiokoulutuksen järjestämisluvat. Palvelusta
+            löytyy lisäksi väestö-, koulutus- ja lupatietoja ja erilaisia
+            tilastoja ammatillisen koulutuksen osalta.
           </p>
-          {/* <Palaute><a href="https://link.webropolsurveys.com/S/F156FEC089139BF4" target="_blank">Palautelomake</a></Palaute> */}
         </InfoBox>
       </ContentContainer>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
