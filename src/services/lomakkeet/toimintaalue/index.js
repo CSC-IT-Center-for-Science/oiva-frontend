5fb9cf2fbb9181365fec69c0ac1de9ea24a6a852
Raimo Pitkänen <rapitkan@gmail.com>
2020-06-01 19:16:59 +0300

    chore: unneeded variables removed

--- src/services/lomakkeet/toimintaalue/index.js
+++ src/services/lomakkeet/toimintaalue/index.js
@@ -1,106 +1,37 @@
-import { isAdded, isRemoved, isInLupa } from "../../../css/label";
 import "../i18n-config";
 import { __ } from "i18n-for-browser";
 
 function getModificationForm(
-  fiCode,
-  maaraysUuid,
+  isEditViewActive,
   changeObjectsByProvince = {},
-  isEiMaariteltyaToimintaaluettaChecked,
-  isMaakunnatJaKunnatChecked,
-  isValtakunnallinenChecked,
   options = [],
   onChanges,
   kunnat,
   maakunnat,
   localizations,
-  isMaakunnatJaKunnatActive
+  toggleEditView
 ) {
   return [
     {
-      anchor: "maakunnat-ja-kunnat",
+      anchor: "valintakentta",
       components: [
         {
-          anchor: "radio",
-          name: "RadioButtonWithLabel",
+          anchor: "maakunnatjakunnat",
+          name: "CategoryFilter",
+          styleClasses: ["mt-4"],
           properties: {
-            isChecked: isMaakunnatJaKunnatChecked,
-            labelStyles: {
-              addition: isAdded,
-              removal: isRemoved
+            anchor: "maakuntakunnat",
+            changeObjectsByProvince,
+            isEditViewActive,
+            localizations,
+            municipalities: kunnat,
+            onChanges: payload => {
+              onChanges(payload);
             },
-            forChangeObject: {
-              title: "Maakunnat ja kunnat"
-            },
-            title: "Maakunnat ja kunnat"
-          }
-        }
-      ],
-      categories: [
-        {
-          anchor: "valintakentta",
-          isVisible: isMaakunnatJaKunnatActive,
-          layout: { indentation: "none" },
-          components: [
-            {
-              anchor: "maakunnatjakunnat",
-              name: "CategoryFilter",
-              styleClasses: ["ml-10 mt-4"],
-              properties: {
-                anchor: "maakuntakunnat",
-                changeObjectsByProvince,
-                localizations,
-                municipalities: kunnat,
-                onChanges: payload => {
-                  onChanges(payload);
-                },
-                provinces: options,
-                provincesWithoutMunicipalities: maakunnat,
-                showCategoryTitles: false
-              }
-            }
-          ]
-        }
-      ]
-    },
-    {
-      anchor: "valtakunnallinen",
-      components: [
-        {
-          anchor: "radio",
-          name: "RadioButtonWithLabel",
-          properties: {
-            title: "Koko Suomi - pois lukien Ahvenanmaan maakunta",
-            isChecked: isValtakunnallinenChecked,
-            labelStyles: {
-              addition: isAdded,
-              custom: fiCode === "FI1" ? isInLupa : {},
-              removal: isRemoved
-            },
-            forChangeObject: {
-              maaraysUuid,
-              title: __("responsibilities")
-            }
-          }
-        }
-      ]
-    },
-    {
-      anchor: "ei-maariteltya-toiminta-aluetta",
-      components: [
-        {
-          anchor: "radio",
-          name: "RadioButtonWithLabel",
-          properties: {
-            isChecked: isEiMaariteltyaToimintaaluettaChecked,
-            labelStyles: {
-              addition: isAdded,
-              removal: isRemoved
-            },
-            forChangeObject: {
-              title: "Ei määriteltyä toiminta-aluetta"
-            },
-            title: "Ei määriteltyä toiminta-aluetta"
+            toggleEditView,
+            provinces: options,
+            provincesWithoutMunicipalities: maakunnat,
+            showCategoryTitles: false
           }
         }
       ]
@@ -112,18 +43,14 @@ export default function getToimintaaluelomake(action, data) {
   switch (action) {
     case "modification":
       return getModificationForm(
-        data.fiCode,
-        data.maaraysUuid,
+        data.isEditViewActive,
         data.changeObjectsByProvince,
-        data.isEiMaariteltyaToimintaaluettaChecked,
-        data.isMaakunnatJaKunnatChecked,
-        data.isValtakunnallinenChecked,
         data.options,
         data.onChanges,
         data.kunnat,
         data.maakunnat,
         data.localizations,
-        data.isMaakunnatJaKunnatActive
+        data.toggleEditView
       );
     default:
       return [];
