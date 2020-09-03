08ba06921704b3d874433dc5cd6ba1248a2ffff7
Raimo Pitkänen <rapitkan@gmail.com>
2020-09-03 09:49:49 +0300

    chore: React table v7 -pohjainen toteutus (#522)
    * chore: React table v7 -pohjainen toteutus
    
    * chore: Lokalisoinneissa merge-ongelmia, joita tässä ratkaistu

--- src/scenes/Jarjestajat/Jarjestajat.js
+++ src/scenes/Jarjestajat/Jarjestajat.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { useLuvat } from "../../stores/luvat";
-import LuvatList from "./components/LuvatList";
+import Jarjestajaluettelo from "./components/Jarjestajaluettelo";
 import { Helmet } from "react-helmet";
 import common from "../../i18n/definitions/common";
 import education from "../../i18n/definitions/education";
 import Loading from "../../modules/Loading";
 import { useIntl } from "react-intl";
-import * as R from "ramda";
 
 const Jarjestajat = React.memo(() => {
   const intl = useIntl();
@@ -32,23 +31,14 @@ const Jarjestajat = React.memo(() => {
       <BreadcrumbsItem to="/">
         {intl.formatMessage(common.frontpage)}
       </BreadcrumbsItem>
-      <BreadcrumbsItem to="/jarjestajat">{intl.formatMessage(education.vocationalEducation)}</BreadcrumbsItem>
+      <BreadcrumbsItem to="/jarjestajat">
+        {intl.formatMessage(education.vocationalEducation)}
+      </BreadcrumbsItem>
 
-      <div className="mx-auto w-full sm:w-3/4 mb-16">
-        <h1>{intl.formatMessage(common.ammatillisenKoulutuksenJarjestajat)}</h1>
-        {luvat.isLoading === false && !luvat.isErroneous && (
-          <React.Fragment>
-            <p className="my-4">
-              {intl.formatMessage(common.voimassaOlevatJarjestamisluvat, {
-                amount: R.length(luvat.data)
-              })}
-            </p>
-
-            <LuvatList luvat={luvat.data} />
-          </React.Fragment>
-        )}
-        {luvat.isLoading && <Loading />}
-      </div>
+      {luvat.isLoading === false && !luvat.isErroneous && (
+        <Jarjestajaluettelo luvat={luvat.data} />
+      )}
+      {luvat.isLoading && <Loading />}
     </React.Fragment>
   );
 });
