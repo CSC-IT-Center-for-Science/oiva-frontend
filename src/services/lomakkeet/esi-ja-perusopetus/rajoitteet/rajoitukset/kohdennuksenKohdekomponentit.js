44423623c7d3dd27f6ed6f8bfcb4b6489dd8d08f
Raimo Pitkänen <rapitkan@gmail.com>
2021-01-11 14:17:23 +0200

    Palautetaan kohdeavaimesta riippumatta samat asiat.

--- src/services/lomakkeet/esi-ja-perusopetus/rajoitteet/rajoitukset/kohdennuksenKohdekomponentit.js
+++ src/services/lomakkeet/esi-ja-perusopetus/rajoitteet/rajoitukset/kohdennuksenKohdekomponentit.js
@@ -1,34 +1,29 @@
-export const getKohdennuksenKohdekomponentti = kohdennuksenKohdeavain => {
-  if (
-    kohdennuksenKohdeavain === "kokonaismaara" ||
-    kohdennuksenKohdeavain === "opiskelijamaarat"
-  ) {
-    return {
-      anchor: "kohde",
-      name: "Autocomplete",
-      layout: { indentation: "none" },
-      styleClasses: ["w-4/5 xl:w-2/3 mb-6"],
-      properties: {
-        isMulti: false,
-        options: [
-          {
-            value: "joistaEnintaan",
-            label: "Joista enintään"
-          },
-          {
-            value: "joistaVahintaan",
-            label: "Joista vähintään"
-          },
-          {
-            value: "lisaksiEnintaan",
-            label: "Lisäksi enintään"
-          },
-          {
-            value: "lisaksiVahintaan",
-            label: "Lisäksi vähintään"
-          }
-        ]
-      }
-    };
-  }
+export const getKohdennuksenKohdekomponentti = () => {
+  return {
+    anchor: "kohde",
+    name: "Autocomplete",
+    layout: { indentation: "none" },
+    styleClasses: ["w-4/5 xl:w-2/3 mb-6"],
+    properties: {
+      isMulti: false,
+      options: [
+        {
+          value: "joistaEnintaan",
+          label: "Joista enintään"
+        },
+        {
+          value: "joistaVahintaan",
+          label: "Joista vähintään"
+        },
+        {
+          value: "lisaksiEnintaan",
+          label: "Lisäksi enintään"
+        },
+        {
+          value: "lisaksiVahintaan",
+          label: "Lisäksi vähintään"
+        }
+      ]
+    }
+  };
 };
