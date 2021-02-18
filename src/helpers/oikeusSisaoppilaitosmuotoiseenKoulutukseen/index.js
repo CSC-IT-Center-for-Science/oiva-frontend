7040c0899c14731c91634bfa7b2f9fba6aa51079
Raimo Pitkänen <rapitkan@gmail.com>
2021-02-18 10:47:29 +0200

    WIP

--- src/helpers/oikeusSisaoppilaitosmuotoiseenKoulutukseen/index.js
+++ src/helpers/oikeusSisaoppilaitosmuotoiseenKoulutukseen/index.js
@@ -1,5 +1,29 @@
-import { groupBy, head, map, mapObjIndexed, omit, prop, sort } from "ramda";
+import {
+  append,
+  drop,
+  mapObjIndexed,
+  groupBy,
+  prop,
+  head,
+  omit,
+  map,
+  sort,
+  find,
+  compose,
+  endsWith,
+  propEq,
+  path,
+  includes,
+  flatten,
+  values,
+  reject,
+  isNil,
+  pathEq,
+  take
+} from "ramda";
 import localforage from "localforage";
+import { __ } from "i18n-for-browser";
+import { createAlimaarayksetBEObjects } from "helpers/rajoitteetHelper";
 
 export const initializeOikeus = oikeus => {
   return omit(["koodiArvo"], {
@@ -25,6 +49,130 @@ export const initializeOikeudet = oikeudet => {
       return initializeOikeus(oikeus);
     }, oikeudet)
   );
+};
+
+export const defineBackendChangeObjects = async (
+  changeObjects = {},
+  maaraystyypit,
+  locale,
+  kohteet
+) => {
+  const { rajoitteetByRajoiteId } = changeObjects;
+
+  const opetuksenJarjestamismuodot = await getOikeusSisaoppilaitosmuotoiseenKoulutukseenFromStorage();
+
+  const kohde = find(propEq("tunniste", "opetuksenjarjestamismuoto"), kohteet);
+  const maaraystyyppi = find(propEq("tunniste", "OIKEUS"), maaraystyypit);
+
+  const opetuksenJarjestamismuotoChangeObjs = map(
+    jarjestamismuoto => {
+      const rajoitteetByRajoiteIdAndKoodiarvo = reject(
+        isNil,
+        mapObjIndexed(rajoite => {
+          return pathEq(
+            [1, "properties", "value", "value"],
+            jarjestamismuoto.koodiarvo,
+            rajoite
+          )
+            ? rajoite
+            : null;
+        }, rajoitteetByRajoiteId)
+      );
+      const changeObj = find(
+        compose(
+          endsWith(`${jarjestamismuoto.koodiarvo}.valinta`),
+          prop("anchor")
+        ),
+        changeObjects.opetuksenJarjestamismuodot
+      );
+      const kuvausChangeObj = find(
+        compose(
+          endsWith(`${jarjestamismuoto.koodiarvo}.kuvaus.A`),
+          prop("anchor")
+        ),
+        changeObjects.opetuksenJarjestamismuodot
+      );
+
+      const muutosobjekti = changeObj
+        ? {
+            generatedId: `opetuksenJarjestamismuoto-${Math.random()}`,
+            kohde,
+            koodiarvo: jarjestamismuoto.koodiarvo,
+            koodisto: jarjestamismuoto.koodisto.koodistoUri,
+            kuvaus: changeObj.properties.value,
+            maaraystyyppi,
+            meta: {
+              changeObjects: [
+                changeObj,
+                kuvausChangeObj,
+                take(2, values(rajoitteetByRajoiteIdAndKoodiarvo))
+              ].filter(Boolean),
+              kuvaus: changeObj.properties.value
+            },
+            tila: changeObj.properties.isChecked ? "LISAYS" : "POISTO"
+          }
+        : null;
+
+      // Muodostetaan tehdyistä rajoittuksista objektit backendiä varten.
+      // Linkitetään ensimmäinen rajoitteen osa yllä luotuun muutokseen ja
+      // loput toisiinsa "alenevassa polvessa".
+      const alimaaraykset = values(
+        mapObjIndexed(asetukset => {
+          return createAlimaarayksetBEObjects(
+            kohteet,
+            maaraystyypit,
+            muutosobjekti,
+            drop(2, asetukset)
+          );
+        }, rajoitteetByRajoiteIdAndKoodiarvo)
+      );
+
+      return [muutosobjekti, alimaaraykset];
+    },
+    append(
+      {
+        koodiarvo: 0,
+        koodisto: { koodistoUri: "opetuksenjarjestamismuoto" },
+        kuvaus: __("education.eiSisaOppilaitosTaiKotikoulumuotoinen")
+      },
+      opetuksenJarjestamismuodot
+    )
+  ).filter(Boolean);
+
+  /**
+   * Lisätiedot-kenttä tulee voida tallentaa ilman, että osioon on tehty muita
+   * muutoksia. Siksi kentän tiedoista luodaan tässä kohtaa oma backend-
+   * muotoinen muutosobjekti.
+   */
+  const lisatiedotChangeObj = find(
+    compose(includes(".lisatiedot."), prop("anchor")),
+    changeObjects.opetuksenJarjestamismuodot
+  );
+
+  const lisatiedotBEchangeObject = lisatiedotChangeObj
+    ? {
+        kohde,
+        koodiarvo: path(
+          ["properties", "metadata", "koodiarvo"],
+          lisatiedotChangeObj
+        ),
+        koodisto: path(
+          ["properties", "metadata", "koodisto", "koodistoUri"],
+          lisatiedotChangeObj
+        ),
+        maaraystyyppi,
+        meta: {
+          arvo: path(["properties", "value"], lisatiedotChangeObj),
+          changeObjects: [lisatiedotChangeObj]
+        },
+        tila: "LISAYS"
+      }
+    : null;
+
+  return flatten([
+    opetuksenJarjestamismuotoChangeObjs,
+    lisatiedotBEchangeObject
+  ]).filter(Boolean);
 };
 
 export function getOikeusSisaoppilaitosmuotoiseenKoulutukseenFromStorage() {
