6e18f1c9eae7950413a38b2c246e912ef19dc606
Raimo Pitkänen <rapitkan@gmail.com>
2021-03-02 15:16:36 +0200

    Pyydetty muutos tehty.

--- src/services/lomakkeet/esi-ja-perusopetus/rajoitteet/rajoitukset/oppilaitokset.js
+++ src/services/lomakkeet/esi-ja-perusopetus/rajoitteet/rajoitukset/oppilaitokset.js
@@ -21,7 +21,7 @@ export default async function getOppilaitokset(
             section: "oppilaitokset"
           },
           inputId,
-          isMulti: true,
+          isMulti: useMultiselect,
           isReadOnly,
           options: map(oppilaitos => {
             return {
