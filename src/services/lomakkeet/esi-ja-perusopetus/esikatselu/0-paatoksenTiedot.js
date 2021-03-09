d7f015d9d775dabaf5d96605898cfdf100e19810
jleppnen <68279869+jleppnen@users.noreply.github.com>
2021-03-09 12:53:30 +0200

    CSCOIVA-1882: Näytetään esikatselussa päätöksen tiedoista päättymispäivämäärä, mikäli se on syötetty (#797)

---
+++ src/services/lomakkeet/esi-ja-perusopetus/esikatselu/0-paatoksenTiedot.js
@@ -0,0 +1,34 @@
+import { __mf } from "i18n-for-browser";
+import { find, path } from "ramda";
+import moment from "moment";
+
+export async function previewOfPaatoksentiedot({ lomakedata }) {
+  const paattymispvm = path(
+    ["properties", "value"],
+    find(
+      cObj => cObj.anchor === "paatoksentiedot.paattymispaivamaara.A",
+      lomakedata || []
+    )
+  );
+
+  return paattymispvm
+    ? {
+        structure: [
+          {
+            anchor: "paattymispvm",
+            components: [
+              {
+                anchor: "A",
+                name: "StatusTextRow",
+                properties: {
+                  code: `${__mf("esiJaPerusopetus.onVoimassa", {
+                    loppupvm: moment(paattymispvm).format("DD.MM.YYYY")
+                  })}`
+                }
+              }
+            ]
+          }
+        ]
+      }
+    : {};
+}
--- src/services/lomakkeet/index.js
+++ src/services/lomakkeet/index.js
@@ -75,6 +75,7 @@ import { previewOfOpiskelijamaarat as previewOfOpiskelijamaaratLukio } from "./l
 import { previewOfMuutEhdot as previewOfMuutEhdotLukio } from "./lukiokoulutus/esikatselu/7-muutEhdot";
 
 import { previewOfRajoite as previewOfRajoiteLukio } from "./lukiokoulutus/esikatselu/10-rajoite";
+import { previewOfPaatoksentiedot } from "./esi-ja-perusopetus/esikatselu/0-paatoksenTiedot";
 
 /**
  * LOMAKEPALVELU
@@ -383,7 +384,9 @@ const lomakkeet = {
     },
     paatoksenTiedot: {
       addition: (data, booleans, locale, changeObjects) =>
-        getPaatoksenTiedot(data, booleans, locale, changeObjects)
+        getPaatoksenTiedot(data, booleans, locale, changeObjects),
+      preview: (data, booleans, locale, changeObjects) =>
+        previewOfPaatoksentiedot(data, booleans, locale, changeObjects)
     },
     opetustaAntavatKunnat: {
       modification: (data, booleans, locale, changeObjects, functions) =>
