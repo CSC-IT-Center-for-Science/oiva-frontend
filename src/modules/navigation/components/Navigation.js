ed5c690a9e47aa00581421bf85464bb046396d4c
vismajj <joona.jalovaara@visma.com>
2021-04-15 09:39:37 +0300

    CSCOIVA-1933: Desktop navigaation muutokset

--- src/modules/navigation/components/Navigation.js
+++ src/modules/navigation/components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { AppRoute } from "const/index";
 import { useIntl } from "react-intl";
 import { isEmpty, length, map } from "ramda";
@@ -10,50 +10,61 @@ import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 export const Navigation = ({ routes }) => {
   const { formatMessage, locale } = useIntl();
-  const [visibleSubMenuRoute, setVisibleSubMenuRoute] = useState();
+  const [visibleSubMenuRoute, setVisibleSubMenuRoute] = useState(null);
+  const location = useLocation();
 
   return (
     !isEmpty(routes) && (
       // 1. tason navigaatio
       <ul className={"flex"}>
         {routes.map(routeObj => {
+          const routeKey = localizeRouteKey(
+            locale,
+            AppRoute[routeObj.key],
+            formatMessage
+          );
+
           return (
             <li
               key={routeObj.key}
-              className="flex flex-col items-center hover:bg-green-600"
-              onClick={() => {
-                setVisibleSubMenuRoute(
-                  visibleSubMenuRoute === null ? routeObj.key : null
-                );
-              }}
+              className="flex flex-col items-center hover:bg-green-600 hover:text-white hover:bg-opacity-50"
               onMouseEnter={() => setVisibleSubMenuRoute(routeObj.key)}
               onMouseLeave={() => setVisibleSubMenuRoute(null)}
             >
-              <NavLink
-                to={localizeRouteKey(
-                  locale,
-                  AppRoute[routeObj.key],
-                  formatMessage
-                )}
-                activeClassName={"bg-green-700 bg-opacity-75"}
-                className="flex flex-col justify-center text-white px-5 h-20 uppercase font-medium hover:text-white hover:bg-green-600 hover:bg-opacity-50"
-                style={{ fontSize: "0.9375rem" }}
+              <div
+                className={`flex flex-col justify-center text-white px-2 h-20 uppercase font-medium hover:text-white ${ location.pathname.indexOf(routeKey) !== -1 && 'bg-green-700' }`}
+                style={{
+                  fontSize: "0.9375rem"
+                }}
               >
-                <div>
-                  <span>
-                    {routeObj.titleKey
-                      ? formatMessage({ id: routeObj.titleKey })
-                      : routeObj.title}
-                  </span>
+                <div className="flex flex-col justify-center hover:text-white" style={{height: "100%", flexDirection: "row"}}>
+                  <NavLink
+                    to={routeKey}
+                    activeClassName={"bg-green-700 bg-opacity-75"}
+                    className="flex flex-col justify-center text-white hover:text-white px-3"
+                  >
+                    <span>
+                      {routeObj.titleKey
+                        ? formatMessage({ id: routeObj.titleKey })
+                        : routeObj.title}
+                    </span>
+                  </NavLink>
                   {length(routeObj.routes) ? (
-                    visibleSubMenuRoute === routeObj.key ? (
-                      <ExpandLessIcon className="ml-2 align-bottom" />
-                    ) : (
-                      <ExpandMoreIcon className="ml-2 align-bottom" />
-                    )
+                    <button
+                      onClick={() => {
+                        setVisibleSubMenuRoute(
+                          visibleSubMenuRoute === null ? routeObj.key : null
+                        );
+                      }}
+                      className="flex flex-col justify-center text-white">
+                      {visibleSubMenuRoute === routeObj.key
+                      ? <ExpandLessIcon className="align-bottom" />
+                      : <ExpandMoreIcon className="align-bottom" /> }
+                    </button>
                   ) : null}
                 </div>
-              </NavLink>
+              </div>
+              {/*</NavLink>*/}
 
               {/* 2. tason navigaatio */}
               {!isEmpty(routeObj.routes) &&
