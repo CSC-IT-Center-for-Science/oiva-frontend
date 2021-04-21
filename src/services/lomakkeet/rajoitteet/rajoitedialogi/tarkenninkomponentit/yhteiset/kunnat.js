59f8e95644de526a3353d6f875b8d536eba1c429
Raimo Pitkänen <rapitkan@gmail.com>
2021-04-21 13:13:38 +0300

    Ulkomaan kunnat näkyvät rajoitteissa.

--- src/services/lomakkeet/rajoitteet/rajoitedialogi/tarkenninkomponentit/yhteiset/kunnat.js
+++ src/services/lomakkeet/rajoitteet/rajoitedialogi/tarkenninkomponentit/yhteiset/kunnat.js
@@ -39,7 +39,7 @@ export default async function getKunnat(
   const ulkomaatStateObj = filter(changeObj => {
     return (
       endsWith(".kuvaus", changeObj.anchor) &&
-      startsWith("toimintaalue.ulkomaa.", changeObj.anchor)
+      startsWith("toimintaalue.200.", changeObj.anchor)
     );
   }, osionData);
 
