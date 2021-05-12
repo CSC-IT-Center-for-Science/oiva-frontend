95b81bf185572824aa8009fe9c8ef17cbfabb431
Raimo Pitkänen <rapitkan@gmail.com>
2021-05-12 14:40:16 +0300

    CSCOIVA-2034: Lomakkeen esikatselun tyhjien osioiden välistykset (#907)

--- src/components/02-organisms/Lomake/index.js
+++ src/components/02-organisms/Lomake/index.js
@@ -168,7 +168,7 @@ const Lomake = React.memo(
 
     if (Array.isArray(lomake) && lomake.length) {
       return (
-        <div>
+        <div className="pt-8">
           {code || formTitle ? (
             <FormTitle
               code={isPreviewModeOn || !code ? null : code}
